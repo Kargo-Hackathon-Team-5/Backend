e55667b6811ae47a95578d96493469a9222e5bd0
haidar.shoesmart <haidar.shoesmart@gmail.com>
2022-08-13 12:00:09 +0700

    route update and create truck

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const driverController = require('./controllers/driver.js')
 const app = express()
 const port = 3000
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/driver/', driverController.createDriver);
+app.put('/driver/:id', driverController.updateDriver);
+// app.post('/driver/', (req, res) => {
+//   res.send({
+//     message: 'Create Driver Successfully',
+//     body:req.body
+//   });
+// })
+
+// app.put('/driver/:id', (req, res) => {
+
+//   const id = req.params.id;
+//   const msg = {
+//     message : 'Update Driver Successfully',
+//     body : req.body
+//   };
+//   res.send(msg)
+// })
 
 app.listen(port, () => {
   console.log(`Success Listening on port ${port}`)
